370cdfb41216a31b44ac8a5f87177bb19f6eccc3
actlikemunyao <munyaopapain@gmail.com>
2025-07-23 14:08:23 +0300

    Update test_utils.py

--- 0x03-Unittests_and_integration_tests/test_utils.py
+++ 0x03-Unittests_and_integration_tests/test_utils.py
@@ -1,9 +1,7 @@
-  #!/usr/bin/env python3  @parameterized.expand([
-        ({}, ("a",)),
-        ({"a": 1}, ("a", "b")),
-    ])
-    def test_access_nested_map_exception(self, nested_map, path):
-        """Test access_nested_map raises KeyError for missing keys"""
-        with self.assertRaises(KeyError) as context:
-            access_nested_map(nested_map, path)
-        self.assertEqual(str(context.exception), repr(path[-1]))
+#!/usr/bin/env python3
+"""
+Unittests for access_nested_map
+"""
+import unittest
+from parameterized import parameterized
+from utils import access_nested_map
