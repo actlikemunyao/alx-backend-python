2e50556342e513c0daa08bb0b0d37e5fb76d84b2
actlikemunyao <munyaopapain@gmail.com>
2025-07-23 14:09:19 +0300

    Update test_utils.py

--- 0x03-Unittests_and_integration_tests/test_utils.py
+++ 0x03-Unittests_and_integration_tests/test_utils.py
@@ -5,3 +5,24 @@ Unittests for access_nested_map
 import unittest
 from parameterized import parameterized
 from utils import access_nested_map
+class TestAccessNestedMap(unittest.TestCase):
+    """Test cases for utils.access_nested_map"""
+
+    @parameterized.expand([
+        ({"a": 1}, ("a",), 1),
+        ({"a": {"b": 2}}, ("a",), {"b": 2}),
+        ({"a": {"b": 2}}, ("a", "b"), 2),
+    ])
+    def test_access_nested_map(self, nested_map, path, expected):
+        """Test valid nested paths"""
+        self.assertEqual(access_nested_map(nested_map, path), expected)
+
+    @parameterized.expand([
+        ({}, ("a",)),
+        ({"a": 1}, ("a", "b")),
+    ])
+    def test_access_nested_map_exception(self, nested_map, path):
+        """Test exceptions on invalid paths"""
+        with self.assertRaises(KeyError) as ctx:
+            access_nested_map(nested_map, path)
+        self.assertEqual(str(ctx.exception), repr(path[-1]))
