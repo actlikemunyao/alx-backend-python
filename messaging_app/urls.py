18b6e68e11584d21d6c3e1be5e37108bc5976e5b
actlikemunyao <munyaopapain@gmail.com>
2025-07-24 13:01:23 +0300

    Update urls.py

--- messaging_app/urls.py
+++ messaging_app/urls.py
@@ -1,2 +1,9 @@
  ["from django.urls import", "path", "include", "routers.DefaultRouter()"]
 ["NestedDefaultRouter", "ConversationViewSet", "MessageViewSet"]
+from django.contrib import admin
+from django.urls import path, include
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('api/', include('chats.urls')),  # 👈 include your app's routes under /api/
+]
