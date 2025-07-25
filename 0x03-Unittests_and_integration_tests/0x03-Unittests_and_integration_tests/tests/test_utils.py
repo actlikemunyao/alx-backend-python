2c47fe2d61a82c0683cbe0655c2f71dafbd4aaf7
actlikemunyao <munyaopapain@gmail.com>
2025-07-25 15:01:40 +0300

    Add files via upload

---
+++ 0x03-Unittests_and_integration_tests/0x03-Unittests_and_integration_tests/tests/test_utils.py
@@ -0,0 +1,57 @@
+#!/usr/bin/env python3
+"""Unit tests for utils.py"""
+
+import unittest
+from parameterized import parameterized
+from unittest.mock import patch
+from utils import access_nested_map, get_json, memoize
+
+class TestAccessNestedMap(unittest.TestCase):
+    """Test access_nested_map"""
+
+    @parameterized.expand([
+        ({"a": 1}, ("a",), 1),
+        ({"a": {"b": 2}}, ("a",), {"b": 2}),
+        ({"a": {"b": 2}}, ("a", "b"), 2)
+    ])
+    def test_access_nested_map(self, nested_map, path, expected):
+        self.assertEqual(access_nested_map(nested_map, path), expected)
+
+    @parameterized.expand([
+        ({}, ("a",), KeyError),
+        ({"a": 1}, ("a", "b"), KeyError)
+    ])
+    def test_access_nested_map_exception(self, nested_map, path, expected_exception):
+        with self.assertRaises(expected_exception):
+            access_nested_map(nested_map, path)
+
+class TestGetJson(unittest.TestCase):
+    """Test get_json"""
+
+    @parameterized.expand([
+        ("http://example.com", {"payload": True}),
+        ("http://holberton.io", {"payload": False})
+    ])
+    def test_get_json(self, test_url, test_payload):
+        with patch("utils.requests.get") as mock_get:
+            mock_get.return_value.json.return_value = test_payload
+            result = get_json(test_url)
+            self.assertEqual(result, test_payload)
+
+class TestMemoize(unittest.TestCase):
+    """Test memoize"""
+
+    def test_memoize(self):
+        class TestClass:
+            def a_method(self):
+                return 42
+
+            @memoize
+            def a_property(self):
+                return self.a_method()
+
+        with patch.object(TestClass, "a_method", return_value=42) as mock_method:
+            test = TestClass()
+            self.assertEqual(test.a_property, 42)
+            self.assertEqual(test.a_property, 42)
+            mock_method.assert_called_once()
