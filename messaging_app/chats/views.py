d4805083b8093148b10059b499b6e106b09535bf
actlikemunyao <munyaopapain@gmail.com>
2025-07-25 14:44:40 +0300

    Update views.py

--- messaging_app/chats/views.py
+++ messaging_app/chats/views.py
@@ -9,3 +9,4 @@ class ConversationViewSet(viewsets.ModelViewSet):
 class MessageViewSet(viewsets.ModelViewSet):
     queryset = Message.objects.all()
     serializer_class = MessageSerializer
+["status", "filters"]
