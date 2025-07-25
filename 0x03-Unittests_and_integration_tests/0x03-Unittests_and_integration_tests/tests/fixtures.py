9d2b700be82dafd745374f225d1b725f37625ad4
actlikemunyao <munyaopapain@gmail.com>
2025-07-25 15:11:58 +0300

    Delete 0x03-Unittests_and_integration_tests/0x03-Unittests_and_integration_tests/tests directory

--- 0x03-Unittests_and_integration_tests/0x03-Unittests_and_integration_tests/tests/fixtures.py
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env python3
-"""Fixtures module"""
-
-TEST_PAYLOAD = (
-    {"repos_url": "https://api.github.com/orgs/test/repos"},
-    [{"name": "repo1", "license": {"key": "apache-2.0"}},
-     {"name": "repo2", "license": {"key": "bsd-3-clause"}}]
-)
