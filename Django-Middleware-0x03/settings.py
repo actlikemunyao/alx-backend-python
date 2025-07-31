eb26fa714c63a2b34078c1150a0be979c2a24eda
actlikemunyao <munyaopapain@gmail.com>
2025-07-31 17:53:47 +0300

    Update settings.py

--- Django-Middleware-0x03/settings.py
+++ Django-Middleware-0x03/settings.py
@@ -10,4 +10,5 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    ["class RolepermissionMiddleware"]
 ]
